d764c0f9fea76a9a65ed8ff1cb4698ed63379a5a
BobH233 <bobh@zendee.cn>
2024-11-06 18:29:43 +0800

    将panel分为子panel

--- addon/panel.py
+++ addon/panel.py
@@ -12,30 +12,61 @@ class BOBH_PT_main_panel(bpy.types.Panel):
     bl_label = 'PhoneController'
     bl_idname = 'BOBH_PT_blender_phone_controller'
 
-    def draw(self, context):
+    def camera_control_sub_panel(self, context, layout):
+        row = layout.row()
+        row.label(text='摄像机控制')
+
+    def armature_control_sub_panel(self, context, layout):
+        row = layout.row()
+        row.label(text='骨骼控制')
+    
+    def object_control_sub_panel(self, context, layout):
+        row = layout.row()
+        row.label(text='物体控制')
+    
+    def server_setting_sub_panel(self, context, layout):
         qr_icon_preview = get_qr_icon_preview()
+        scene = context.scene
+
+        row = layout.row()
+        row.prop(scene, 'default_ws_ip')
+        row = layout.row()
+        row.prop(scene, 'default_ws_port')
+        row = layout.row()
+        row.operator('bobh.start_websocket_server', text='启动WS服务')
+        row = layout.row()
+        row.operator('bobh.stop_websocket_server', text='关闭WS服务')
+        row = layout.row()
+        row.prop(scene, 'default_web_port')
+        row = layout.row()
+        row.operator('bobh.start_webui_server', text='启动Web服务')
+        row = layout.row()
+        row.operator('bobh.stop_webui_server', text='关闭Web服务')
+        if qr_icon_preview.get('qr_image'):
+            row = layout.row()
+            row.label(text='扫描二维码:')
+            row = layout.row()
+            row.template_icon(icon_value=qr_icon_preview['qr_image'].icon_id, scale=6)
+
+    def draw(self, context):
         layout = self.layout
         scene = context.scene
 
         box = layout.box()
         row = box.row()
         row.label(text='服务器设置', icon='SHADING_WIRE')
+        self.server_setting_sub_panel(context, box)
+
+
+        box = layout.box()
         row = box.row()
-        row.prop(scene, 'default_ws_ip')
+        row.label(text='工作模式设定', icon='TOOL_SETTINGS')
         row = box.row()
-        row.prop(scene, 'default_ws_port')
-        row = box.row()
-        row.operator('bobh.start_websocket_server', text='启动WS服务')
-        row = box.row()
-        row.operator('bobh.stop_websocket_server', text='关闭WS服务')
-        row = box.row()
-        row.prop(scene, 'default_web_port')
-        row = box.row()
-        row.operator('bobh.start_webui_server', text='启动Web服务')
-        row = box.row()
-        row.operator('bobh.stop_webui_server', text='关闭Web服务')
-        if qr_icon_preview.get('qr_image'):
-            row = box.row()
-            row.label(text='扫描二维码:')
-            row = box.row()
-            row.template_icon(icon_value=qr_icon_preview['qr_image'].icon_id, scale=6)
+        row.prop(scene, 'work_mode_option', expand=True)
+
+        if scene.work_mode_option == 'CameraMode':
+            self.camera_control_sub_panel(context, box)
+        elif scene.work_mode_option == 'ObjectMode':
+            self.object_control_sub_panel(context, box)
+        elif scene.work_mode_option == 'ArmatureMode':
+            self.armature_control_sub_panel(context, box)
