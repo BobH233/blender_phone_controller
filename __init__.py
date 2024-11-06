8b03b29ac722729335ec4b805ab9c4dc1cc0ae7d
BobH233 <bobh@zendee.cn>
2024-11-06 18:29:35 +0800

    新增工作模式设定的属性，不在插件开始时加载timer

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ try:
     from .addon.webui import stop_server as stop_web_server
     from .addon.panel import BOBH_PT_main_panel
     from .addon.operators import BOBH_OT_start_websocket_server,    BOBH_OT_stop_websocket_server, BOBH_OT_start_webui_server, BOBH_OT_stop_webui_server
-    from .addon.server import update_timer
 except Exception as e:
     print('Warning: Not in blender env!')
 
@@ -32,6 +31,26 @@ def unregister_string_props():
     del bpy.types.Scene.default_ws_port
     del bpy.types.Scene.default_web_port
 
+
+def update_work_mode_option(self, context):
+    print('当前工作模式:', bpy.context.scene.work_mode_option)
+
+def register_enum_props():
+    bpy.types.Scene.work_mode_option = bpy.props.EnumProperty(
+        name='插件工作模式',
+        description='选择插件的工作模式',
+        items=[
+            ('CameraMode', '摄像机控制', '使用移动设备控制摄像机的位姿', 'OUTLINER_OB_CAMERA', 0),
+            ('ArmatureMode', '骨骼控制', '使用移动设备控制骨骼的旋转', 'OUTLINER_OB_ARMATURE', 1),
+            ('ObjectMode', '物体', '使用移动设备控制骨骼的位姿', 'MATCUBE', 2),
+        ],
+        default='CameraMode',
+        update=update_work_mode_option  # 选项更改时的回调函数
+    )
+
+def unregister_enum_props():
+    del bpy.types.Scene.work_mode_option
+
 def register():
     # 注册所有的 class 和属性
     bpy.utils.register_class(BOBH_PT_main_panel)
@@ -40,17 +59,17 @@ def register():
     bpy.utils.register_class(BOBH_OT_start_webui_server)
     bpy.utils.register_class(BOBH_OT_stop_webui_server)
     register_string_props()
-    bpy.app.timers.register(update_timer)
+    register_enum_props()
 
 def unregister():
     # 注销所有的 class 和属性
     bpy.utils.unregister_class(BOBH_PT_main_panel)
     bpy.utils.unregister_class(BOBH_OT_start_websocket_server)
     bpy.utils.unregister_class(BOBH_OT_stop_websocket_server)
-    unregister_string_props()
     bpy.utils.unregister_class(BOBH_OT_start_webui_server)
     bpy.utils.unregister_class(BOBH_OT_stop_webui_server)
-    bpy.app.timers.unregister(update_timer)
+    unregister_string_props()
+    unregister_enum_props()
     stop_ws_server()
     stop_web_server()
 
